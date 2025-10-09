bbb5bc122219a65cceabb2c6056fca753ed52ee5
sofia <ssarmiento088@gmail.com>
2025-10-08 22:37:46 -0500

    feat: update promociones db context

--- Data/AppDBContext.cs
+++ Data/AppDBContext.cs
@@ -9,5 +9,6 @@ public class AppDBContext : DbContext
     public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) { }
 
     public DbSet<Promocion> Promociones { get; set; }
+    public DbSet<Producto> Productos { get; set; }
 
 }
